7fcbc4a378816858b4cd8c2fde0b74ca8292721c
Md Farhan <mdfstar3000@gmail.com>
2025-06-06 14:46:52 +0530

    changed

--- src/App.jsx
+++ src/App.jsx
@@ -7,9 +7,6 @@ import About from './pages/About';
 import Home from './pages/Home';
 import Rewards from './pages/Rewards';
 import PastExams from './pages/PastExams';
-<<<<<<< HEAD
-import EnrollExams from './pages/EnrollExams';
-=======
 
 import EnrollExams from './pages/EnrollExams';
 
@@ -17,10 +14,6 @@ import PrivateComponent from './pages/privateComponent';
 // import EnrollExams from './pages/EnrollExams';
 
 
-
-
-
->>>>>>> 748fdd74a0bd09fa40f5d5505872e84fa5c885f5
 // import CheckResults from './pages/CheckResults';
 import CardLayout from './pages/Exams';
 
@@ -33,17 +26,6 @@ function App() {
          
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
-<<<<<<< HEAD
-          <Route path="/resources" element={<Resources />} />
-          <Route path="/exams" element={<CardLayout/>} />
-          <Route path="/pastexams" element={<PastExams />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/enroll-exams" element={<EnrollExams />} />
-          <Route path="/rewards" element={<Rewards />} />
-          {/* <Route path="/check-results" element={<CheckResults />} /> */}
-          {/* <Route path="/past-exams" element={<PastExams />} /> */}
-=======
 
           
           {/* <Route element={<PrivateComponent />}> */}
@@ -54,13 +36,13 @@ function App() {
             <Route path="/pastexams" element={<PastExams />} />
             <Route path="/contact" element={<Contact />} />
             <Route path="/about" element={<About />} />
-            {/* <Route path="/enroll-exams" element={<EnrollExams />} /> */}
+            <Route path="/enroll-exams" element={<EnrollExams />} />
             <Route path="/rewards" element={<Rewards />} />
             {/* <Route path="/check-results" element={<CheckResults />} /> */}
             
           {/* </Route> */}
 
->>>>>>> 748fdd74a0bd09fa40f5d5505872e84fa5c885f5
+
         </Routes>
       </Router>
     </div>
