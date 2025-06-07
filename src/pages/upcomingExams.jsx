d83a7e51cf7a813f21429eeb2d0a4098471d995e
Rizwan <mdrizwan6386@gmail.com>
2025-06-07 13:36:41 +0530

    correted css import

--- src/pages/upcomingExams.jsx
+++ src/pages/upcomingExams.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import Navbar from '../Components/Navbar';
 import Footer from '../Components/Footer';
-import './UpcomingExams.css';
+import './upcomingExams.css';
 
 const upcomingExams = [
   {
